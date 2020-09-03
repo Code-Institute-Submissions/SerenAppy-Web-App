8dc5184506a1fafde8d4913172a0b041210783ac
Marc Gulliver <Marc.gulliver100@gmail.com>
2020-09-03 09:26:09 +0000

    Buttons text on mediatation pages change to black when on hover and removed the link text decoration on the Main Menu button.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -149,17 +149,4 @@ function formatTime(time) {
 
 
 
-// let video = document.getElementById("myVideo");
-// let btn = document.getElementById("myBtn");
-
-
-// function myFunction() {
-//   if (video.paused) {
-//     video.play();
-//     btn.innerHTML = "Pause";
-//   } else {
-//     video.pause();
-//     btn.innerHTML = "Play";
-//   }
-// }
 
